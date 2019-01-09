d0bb80382e6fdf87f702c97869642dff01dff51b
elvin <dumanxiang@wancloud.io>
2019-01-09 16:27:10 +0800

    add the config of log level

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"validator-monitor/app"
 	_ "validator-monitor/routers"
+	"validator-monitor/utils"
 
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	beego.SetLogFuncCall(true)
+	beego.SetLevel(utils.LogLevel())
 }
 
 func main() {
--- utils/util.go
+++ utils/util.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"github.com/astaxie/beego"
+
 	amino "github.com/tendermint/go-amino"
 	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
@@ -10,3 +12,23 @@ func init() {
 }
 
 var CDC = amino.NewCodec()
+
+func LogLevel() int {
+	levelStr := beego.AppConfig.String("log::level")
+	if "" == levelStr {
+		return beego.LevelDebug
+	}
+
+	switch levelStr {
+	case "debug":
+		return beego.LevelDebug
+	case "info":
+		return beego.LevelInformational
+	case "warning":
+		return beego.LevelWarning
+	case "error":
+		return beego.LevelError
+	}
+
+	return beego.LevelDebug
+}
